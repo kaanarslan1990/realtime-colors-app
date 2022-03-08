49c631e1dc914492d674610079d5d702d913bcbe
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-03-08 19:37:24 +0300

    create realtime app and connected server

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import "./App.css";
+import Palatte from "./components/Palatte";
+import { init } from "./socketApi";
+
+function App() {
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <div className="App">
+      <Palatte />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Palatte.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Palatte() {
+  return <div className="palatte">
+      <input type="color" />
+      <button>Click</button>
+  </div>;
+}
+
+export default Palatte;
