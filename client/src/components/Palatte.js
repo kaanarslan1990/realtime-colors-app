3677826aea930a8801464f91e5eec62d12730305
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-03-08 21:21:01 +0300

    send data to backend from ui

--- client/src/components/Palatte.js
+++ client/src/components/Palatte.js
@@ -1,9 +1,15 @@
-import React from "react";
+import {useState} from "react";
+import { send } from "../socketApi";
 
 function Palatte() {
+  const [color, setColor] = useState("#000000");
+
   return <div className="palatte">
-      <input type="color" />
-      <button>Click</button>
+      <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+      <button onClick={() => send(color)}>Click</button>
+      {
+        color
+      }
   </div>;
 }
 
--- client/src/socketApi.js
+++ client/src/socketApi.js
@@ -3,13 +3,15 @@ import io from "socket.io-client";
 let socket;
 
 export const init = () => {
-	console.log("Connecting...");
+  console.log("Connecting...");
 
-	socket = io("http://localhost:3001", {
-		transports: ["websocket"],
-	});
+  socket = io("http://localhost:3001", {
+    transports: ["websocket"],
+  });
 
-	socket.on("connect", () =>
-		console.log("Connected succesfuly.")
-	);
+  socket.on("connect", () => console.log("Connected succesfuly."));
+};
+
+export const send = (color) => {
+  socket.emit("newColor", color);
 };
